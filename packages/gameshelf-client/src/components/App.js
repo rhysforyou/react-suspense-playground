3e1db7d6cd56b4c8c17ef1e3184a10aeea27fa10
Rhys Powell <rhys@rpowell.me>
2018-10-31 20:40:42 +1100

    use React.lazy to load modules

--- packages/gameshelf-client/src/components/App.js
+++ packages/gameshelf-client/src/components/App.js
@@ -2,24 +2,21 @@ import React from "react";
 import { Router } from "@reach/router";
 import lazyLoadComponent from "../lib/lazyLoadComponent";
 import GameDetails from "./GameDetails";
+import Spinner from "./Spinner";
 
-const Header = lazyLoadComponent(() =>
-  import("./Header").then(mod => mod.default)
-);
-const GamesList = lazyLoadComponent(() =>
-  import("./GamesList").then(mod => mod.default)
-);
+const Header = React.lazy(() => import("./Header"));
+const GamesList = React.lazy(() => import("./GamesList"));
 
 export default class App extends React.Component {
   render() {
     return (
-      <>
+      <React.Suspense fallback={<Spinner />}>
         <Header />
         <Router>
           <GamesList path="/" />
           <GameDetails path="/games/:gameId" />
         </Router>
-      </>
+      </React.Suspense>
     );
   }
 }
